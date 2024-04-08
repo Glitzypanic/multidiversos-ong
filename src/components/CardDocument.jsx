fdf5ca4bf0334dd8555406c103643efd84d88926
Jose Farias <jose.fariasdv@gmail.com>
2024-04-08 15:49:18 -0700

    Refactor CSS styles, update component imports, and add new components and styles

---
+++ src/components/CardDocument.jsx
@@ -0,0 +1,42 @@
+import DownloadIcon from "./svg/Download";
+import ExternalLink from "./svg/ExternalLink";
+import DocumentImg from "../../public/img/documentsImg/pdf-multidiversos.png";
+import "../styles/CardDocument.css";
+
+function CardDocument() {
+  return (
+    <>
+        <div className="card-document">
+          <img
+            className="card-document-img"
+            src={DocumentImg}
+            alt="Portadad del pdf llamado La inclusion la haces tú"
+          />
+          <div className="card-document-info">
+            <h5 className="card-document-tag">PDF</h5>
+            <h2 className="card-document-title">Memoria Conversatorio Departamento de Turismo: La inclusión la haces tú </h2>
+            <p className="card-document-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              officiis eveniet, asperiores deserunt harum magni nobis, fugit
+              reiciendis libero ipsam autem blanditiis perspiciatis. Iusto optio
+              porro tempore inventore! Porro, dolore.
+            </p>
+            <div className="card-document-buttons">
+              <button className="card-document-button">
+                <a href="#" className="card-document-link">
+                  Descargar <DownloadIcon />
+                </a>
+              </button>
+              <button className="card-document-button">
+                <a href="#" className="card-document-link">
+                  Ver <ExternalLink />
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+}
+
+export default CardDocument;
--- src/pages/DocumentsPage.jsx
+++ src/pages/DocumentsPage.jsx
@@ -2,27 +2,29 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import NavMenu from "../components/NavMenu";
 import Banner from "../assets/banner.webp";
+import CardDocument from "../components/CardDocument";
 
-import "../styles/DocumentsPage.css"
+import "../styles/DocumentsPage.css";
 
 export default function DocumentsPage() {
   return (
     <>
-        <Header />
-        <NavMenu />
-        <div className="banner">
-          <img src={Banner} alt="" />
-        </div>
-        <div className="container">
-            <h1>Documentos</h1>
-            <p>Aqui puedes encontrar material para ver y descargar.</p>
-            <div className="document">
-                <h2>Documento 1</h2>
-                <p>Descripción del documento 1</p>
-                <a href="#">Descargar</a>
-            </div>
-        </div>
-        <Footer />
+      <Header />
+      <NavMenu />
+      <div className="banner">
+        <img src={Banner} alt="" />
+      </div>
+      <div className="document-container">
+        <h5 className="document-container-tag">DOCUMENTOS</h5>
+        <h1 className="document-container-title">Documentos</h1>
+        <p className="document-container-description">
+          Aqui puedes encontrar material para ver y descargar.
+        </p>
+        <CardDocument />
+        <CardDocument />
+        <CardDocument />
+      </div>
+      <Footer />
     </>
   );
 }
