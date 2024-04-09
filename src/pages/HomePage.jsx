f7f40321bae0055cbcb290508048fe1f57636e2e
Jose Farias <jose.fariasdv@gmail.com>
2024-04-09 14:32:12 -0700

    Refactor CSS styles in Mision.css, App.css, Carousel.css, and HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,44 +23,58 @@ export default function HomePage() {
       <section className="home-container">
         <section className="focus-container">
           <h5 className="focus-tag">INICIO</h5>
+          <section className="about">
+          <h1 className="about-title">Multidiversos: Forjando un Mundo Más Inclusivo desde 2020</h1>
+          <p className="about-description">
+            Somos una ONG llamada Multidiversos, conformada por estudiantes y
+            profesionales de diversas áreas, tales como Trabajo Social,
+            Psicología, Administración, Fonoaudiología, Gestión en Turismo y
+            Cultura, Terapia Ocupacional, entre otras.
+          </p>
+          <p className="about-description">
+            Desde mayo del año 2020, hemos estado llevando a cabo diversas
+            actividades con el objetivo de promover una conciencia colectiva y
+            facilitar información sobre la diversidad funcional, así como crear
+            espacios inclusivos para personas con diversidad funcional en las
+            comunas de Valparaíso y Viña del Mar. Estas actividades incluyen la
+            realización de ferias, charlas y talleres dirigidos a equipos de
+            salud y educación.
+          </p>
+          <p className="about-description">
+            Además, hemos colaborado con estudiantes en prácticas de trabajo
+            social y psicología de varias universidades, así como participado en
+            conversatorios y talleres relacionados con la inclusión social.
+            También hemos brindado apoyo a estudiantes con proyectos de
+            capacitación interna.
+          </p>
+          <div className="carousel-container">
+            <Carousel data={carouselData.carousel3} />
+            <span data={carouselData.carousel3.description}></span>
+          </div>
+        </section>
           <h1 className="focus-title">Mision y vision</h1>
           <Mision
+            className="focus-mision"
             src={MisionIcon}
             title="Mision"
             description="Nuestra misión es generar cambios en la sociedad a través de un trabajo interdisciplinario, promoviendo un cambio de lenguaje, fomentando una sociedad inclusiva basada en los Derechos Humanos, disminuyendo barreras, visibilizando situaciones que viven a diario las personas con diversidad funcional, a través de la educación y concientización de la sociedad. "
           />
           <Mision
+            className="focus-mision"
             src={VisionIcon}
             title="Vision"
             description="Nuestra visión es lograr una transformación social respecto a la mirada que se posee sobre la diversidad funcional, que permita una sociedad inclusiva, no discriminatoria y que acepte la diversidad.  Esperamos que a través de nuestro trabajo se hagan valer los derechos de todas las personas con diversidad funcional. "
           />
           <Mision
+            className="focus-mision"
             src={ObjetivoIcon}
             title="Objetivo General"
             description="Nuestro objetivo principal es fomentar una sociedad inclusiva basada en los derechos humanos de las personas, disminuyendo las barreras, cambiando el lenguaje y visibilizando situaciones en torno a la diversidad funcional."
           />
         </section>
 
-        <section className="about">
-          <h1 className="about-title">¿Quiénes somos?</h1>
-
-          <div className="carousel-container">
-            <Carousel data={carouselData.carousel1} />
-            <span data={carouselData.carousel1.description}></span>
-          </div>
-
-          <p  className="about-description">
-            Somos una ONG llamada Multidiversos. Estamos conformados por
-            estudiantes y profesionales de diversas áreas, tales como Trabajo
-            Social, Psicología, Administración, Fonoaudiología, Gestión en
-            Turismo y Cultura, Terapia Ocupacional, entre otras.
-          </p>
-        </section>
-        <div className="form">
-          <h2>Quieres ser parte de nuestro equipo?</h2>
-          <p>Rellena este formulario!</p>
-          <button>Presiona aqui!</button>
-        </div>
+        
+        
         <section className="teamwork-container">
           <div className="title">
             <h2>Les presentamos parte del equipo...</h2>
@@ -80,6 +94,11 @@ export default function HomePage() {
           </div>
           <TeamCard data={teamData.teamCuentas} />
         </section>
+        <div className="form">
+          <h2>Quieres ser parte de nuestro equipo?</h2>
+          <p>Rellena este formulario!</p>
+          <button>Presiona aqui!</button>
+        </div>
       </section>
       <Footer />
     </>
