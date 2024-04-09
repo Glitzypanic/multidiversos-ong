77960faab8f08a68e67b9bd89d3d5c10dc7e8bc6
Jose Farias <jose.fariasdv@gmail.com>
2024-04-09 03:15:12 -0700

    Adding styles to component, refactor css, add Svgs

---
+++ src/components/Mision.jsx
@@ -0,0 +1,17 @@
+import "../styles/Mision.css"
+
+function Mision({ src, title, description  }) {
+  return (
+    <>
+      <div className="focus-mision-card">
+        <img className="focus-mision-img" src={src} alt="" />
+        <h2 className="focus-mision-title">{title}</h2>
+        <p className="focus-mision-description">
+          {description}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Mision;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,6 +6,10 @@ import TeamCard from "../components/TeamCard";
 import Header from "../components/Header";
 import NavMenu from "../components/NavMenu";
 import Footer from "../components/Footer";
+import MisionIcon from "../assets/mision.svg";
+import VisionIcon from "../assets/vision.svg";
+import ObjetivoIcon from "../assets/objetivogeneral.svg";
+import Mision from "../components/Mision";
 
 export default function HomePage() {
   return (
@@ -16,102 +20,66 @@ export default function HomePage() {
         <img src={BannerImg} alt="Banner de Multidiversos" />
       </div>
 
-      <section className="about">
-        <h1>¿Quiénes somos?</h1>
+      <section className="home-container">
+        <section className="focus-container">
+          <h5 className="focus-tag">INICIO</h5>
+          <h1 className="focus-title">Mision y vision</h1>
+          <Mision
+            src={MisionIcon}
+            title="Mision"
+            description="Nuestra misión es generar cambios en la sociedad a través de un trabajo interdisciplinario, promoviendo un cambio de lenguaje, fomentando una sociedad inclusiva basada en los Derechos Humanos, disminuyendo barreras, visibilizando situaciones que viven a diario las personas con diversidad funcional, a través de la educación y concientización de la sociedad. "
+          />
+          <Mision
+            src={VisionIcon}
+            title="Vision"
+            description="Nuestra visión es lograr una transformación social respecto a la mirada que se posee sobre la diversidad funcional, que permita una sociedad inclusiva, no discriminatoria y que acepte la diversidad.  Esperamos que a través de nuestro trabajo se hagan valer los derechos de todas las personas con diversidad funcional. "
+          />
+          <Mision
+            src={ObjetivoIcon}
+            title="Objetivo General"
+            description="Nuestro objetivo principal es fomentar una sociedad inclusiva basada en los derechos humanos de las personas, disminuyendo las barreras, cambiando el lenguaje y visibilizando situaciones en torno a la diversidad funcional."
+          />
+        </section>
 
-        <div className="carousel-container">
-          <Carousel data={carouselData.carousel1} />
-          <span data={carouselData.carousel1.description}></span>
+        <section className="about">
+          <h1 className="about-title">¿Quiénes somos?</h1>
+
+          <div className="carousel-container">
+            <Carousel data={carouselData.carousel1} />
+            <span data={carouselData.carousel1.description}></span>
+          </div>
+
+          <p  className="about-description">
+            Somos una ONG llamada Multidiversos. Estamos conformados por
+            estudiantes y profesionales de diversas áreas, tales como Trabajo
+            Social, Psicología, Administración, Fonoaudiología, Gestión en
+            Turismo y Cultura, Terapia Ocupacional, entre otras.
+          </p>
+        </section>
+        <div className="form">
+          <h2>Quieres ser parte de nuestro equipo?</h2>
+          <p>Rellena este formulario!</p>
+          <button>Presiona aqui!</button>
         </div>
-
-        <p>
-          Somos una ONG llamada Multidiversos. Estamos conformados por
-          estudiantes y profesionales de diversas áreas, tales como Trabajo
-          Social, Psicología, Administración, Fonoaudiología, Gestión en Turismo
-          y Cultura, Terapia Ocupacional, entre otras.
-        </p>
-
-        <div className="carousel-container">
-          <Carousel data={carouselData.carousel2} />
-        </div>
-
-        <p>
-          Desde mayo del año 2020 estamos realizando actividades con el fin de
-          generar una conciencia colectiva, facilitar información, generar
-          espacios para personas con diversidad funcional, dar orientaciones y
-          difundir información relevante a la diversidad funcional y la
-          inclusión social.
-        </p>
-
-        <div className="carousel-container">
-          <Carousel data={carouselData.carousel3} />
-          <span></span>
-        </div>
-
-        <p>
-          Hemos realizado y participado en ferias en las comunas de Valparaíso y
-          Viña del Mar
-        </p>
-
-        <div className="carousel-container">
-          <Carousel data={carouselData.carousel4} />
-        </div>
-
-        <p>
-          Hemos recibido estudiantes en práctica de trabajo social y psicología
-          de diferentes universidades
-        </p>
-
-        <div className="carousel-container">
-          <Carousel data={carouselData.carousel5} />
-        </div>
-
-        <p>
-          Hemos recibido estudiantes en práctica de trabajo social y psicología
-          de diferentes universidades
-        </p>
-
-        <div className="carousel-container">
-          <Carousel data={carouselData.carousel6} />
-        </div>
-
-        <p>
-          Hemos recibido estudiantes en práctica de trabajo social y psicología
-          de diferentes universidades
-        </p>
-
-        <div className="carousel-container">
-          <Carousel data={carouselData.carousel7} />
-        </div>
-
-        <p>
-          Hemos recibido estudiantes en práctica de trabajo social y psicología
-          de diferentes universidades
-        </p>
-      </section>
-      <div className="form">
-        <h2>Quieres ser parte de nuestro equipo?</h2>
-        <p>Rellena este formulario!</p>
-        <button>Presiona aqui!</button>
-      </div>
-      <section className="teamwork-container">
-        <div className="title">
-          <h2>Les presentamos parte del equipo...</h2>
-          <h4>Junta Directiva:</h4>
-        </div>
-        <TeamCard data={teamData.team} />
-      </section>
-      <section className="teamwork-container">
-        <div className="title">
-          <h4>Comisión de ética:</h4>
-        </div>
-        <TeamCard data={teamData.teamEtica} />
-      </section>
-      <section className="teamwork-container">
-        <div className="title">
-          <h4>Comisión Revisora de Cuentas:</h4>
-        </div>
-        <TeamCard data={teamData.teamCuentas} />
+        <section className="teamwork-container">
+          <div className="title">
+            <h2>Les presentamos parte del equipo...</h2>
+            <h4>Junta Directiva:</h4>
+          </div>
+          <TeamCard data={teamData.team} />
+        </section>
+        <section className="teamwork-container">
+          <div className="title">
+            <h4>Comisión de ética:</h4>
+          </div>
+          <TeamCard data={teamData.teamEtica} />
+        </section>
+        <section className="teamwork-container">
+          <div className="title">
+            <h4>Comisión Revisora de Cuentas:</h4>
+          </div>
+          <TeamCard data={teamData.teamCuentas} />
+        </section>
       </section>
       <Footer />
     </>
