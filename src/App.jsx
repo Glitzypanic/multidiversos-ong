f84b5e954e8800903e198a243cfa7682af9ae5b2
Glitzypanic <jose.fariasdv@gmail.com>
2024-04-19 15:06:18 -0700

    Refactor lazy loading of page components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,50 @@
-// import { lazy, Suspense } from "react";
+import React from "react";
+import { Suspense } from "react";
 import { MenuContext } from "./utils/MenuContext.js";
 import { useState } from "react";
 import ScrollToTop from "./components/ScrollToTop.jsx";
 
 import "./styles/pages_style/App.css";
 
-// import { Router } from "./components/Router.jsx";
-// import { Route } from "./components/Route.jsx";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Tools from "./components/Tools.jsx";
 
-// import dinamico de las paginas
-// const lazyPage404 = lazy(() => import("./pages/Page404.jsx"));
-// const lazyHomePage = lazy(() => import("./pages/HomePage.jsx"));
-// const lazyArticlePage = lazy(() => import("./pages/ArticlePage.jsx"));
-// const lazyActivities2020 = lazy(() => import("./pages/Activities2020.jsx"));
-// const lazyActivities2021 = lazy(() => import("./pages/Activities2021.jsx"));
-// const lazyActivities2022 = lazy(() => import("./pages/Activities2022.jsx"));
-// const lazyActivities2023 = lazy(() => import("./pages/Activities2023.jsx"));
-// const lazyOrganigramPage = lazy(() => import("./pages/OrganigramPage.jsx"));
-// const lazyDocumentsPage = lazy(() => import("./pages/DocumentsPage.jsx"));
-
-import lazyPage404 from "./pages/Page404.jsx";
-import lazyHomePage from "./pages/HomePage.jsx";
-import lazyArticlePage from "./pages/ArticlePage.jsx";
-import lazyActivities2020 from "./pages/Activities2020.jsx";
-import lazyActivities2021 from "./pages/Activities2021.jsx";
-import lazyActivities2022 from "./pages/Activities2022.jsx";
-import lazyActivities2023 from "./pages/Activities2023.jsx";
-import lazyOrganigramPage from "./pages/OrganigramPage.jsx";
-import lazyDocumentsPage from "./pages/DocumentsPage.jsx";
-
+const HomePage = React.lazy(() => import("./pages/HomePage.jsx"));
+const ArticlePage = React.lazy(() => import("./pages/ArticlePage.jsx"));
+const Activities2020 = React.lazy(() => import("./pages/Activities2020.jsx"));
+const Activities2021 = React.lazy(() => import("./pages/Activities2021.jsx"));
+const Activities2022 = React.lazy(() => import("./pages/Activities2022.jsx"));
+const Activities2023 = React.lazy(() => import("./pages/Activities2023.jsx"));
+const OrganigramPage = React.lazy(() => import("./pages/OrganigramPage.jsx"));
+const DocumentsPage = React.lazy(() => import("./pages/DocumentsPage.jsx"));
+const Page404 = React.lazy(() => import("./pages/Page404.jsx"));
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
 
   return (
-    <MenuContext.Provider value={{ isMenuOpen, setIsMenuOpen }} className="main-content">
+    <MenuContext.Provider
+      value={{ isMenuOpen, setIsMenuOpen }}
+      className="main-content"
+    >
       <Header />
       <main>
-            <ScrollToTop />
-          <Routes defaultComponent={lazyPage404}>
-            <Route path="/" Component={lazyHomePage} />
-            <Route path="/article" Component={lazyArticlePage} />
-            <Route path="/activities2020" Component={lazyActivities2020} />
-            <Route path="/activities2021" Component={lazyActivities2021} />
-            <Route path="/activities2022" Component={lazyActivities2022} />
-            <Route path="/activities2023" Component={lazyActivities2023} />
-            <Route path="/organigram" Component={lazyOrganigramPage} />
-            <Route path="/documents" Component={lazyDocumentsPage} />
+        <Suspense fallback={Page404}>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" Component={HomePage} />
+            <Route path="/article" Component={ArticlePage} />
+            <Route path="/activities2020" Component={Activities2020} />
+            <Route path="/activities2021" Component={Activities2021} />
+            <Route path="/activities2022" Component={Activities2022} />
+            <Route path="/activities2023" Component={Activities2023} />
+            <Route path="/organigram" Component={OrganigramPage} />
+            <Route path="/documents" Component={DocumentsPage} />
           </Routes>
-      <Tools />
+        </Suspense>
+        <Tools />
       </main>
       <Footer />
     </MenuContext.Provider>
