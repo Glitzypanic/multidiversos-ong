440efde0a1011683da940ddfa4a02713676609bb
glitzypanic <jose.fariasdv@gmail.com>
2024-06-06 20:30:56 -0700

    refactor: improve code readability and add search functionality to SearchPage component

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 
+// Componente de la página de búsqueda
 export default function SearchPage({ routeParams }) {
-    useEffect(() => {
-        document.title = `Has Buscado ${routeParams.query}`;
-        
-        fetch(`https://api.ajdsskadkasd.com/search/${routeParams.query}`)
-    });
+  // Efecto para cambiar el título de la página
+  useEffect(() => {
+    document.title = `Has Buscado ${routeParams.query}`;
+
+    fetch(`https://api.ajdsskadkasd.com/search/${routeParams.query}`);
+  });
 
   return (
     <div>
